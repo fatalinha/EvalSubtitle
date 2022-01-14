c17a0bf0afe444fbc87693b380cf92292b30d33c
FBuet <buet@limsi.fr>
2022-01-14 04:25:19 +0100

    change ter_eval bc of <-eox-> and *-*-*-* tokenisation

--- src/evalsub/ter_eval.py
+++ src/evalsub/ter_eval.py
@@ -1,44 +1,66 @@
 # coding: utf-8
+
+DESCRIPTION = """
+Computes TER_br with and without replacement of type of breaks
+"""
+
+import re
+
 from sacrebleu.metrics import TER
 
-DESCRIPTION = """Computes TER_br with and without replacement of type of breaks"""
+from util.util import preprocess
 
 
-def mask(infile, replace=False, remove_eol=False, remove_eob=False):
-    # Masks all words with ****, returns masked file
-    outlines = []
-    with open(infile) as f:
-        for line in f:
-            if remove_eol and not remove_eob:
-                line = line.replace(' <eol>','')
-            elif remove_eob and not remove_eol:
-                line = line.replace(' <eob>','')
-            elif replace:
-                line = line.replace('<eol>','<eob>')
-            line = line.strip().split(' ')
-            words = []
-            for w in line:
-                if w != '<eob>' and w != '<eol>':
-                    words.append('****')
-                else:
-                    words.append(w)
-            outlines.append(' '.join(words))
-        return outlines
+MASK_CHAR = '#'
+LINE_TAG = '<eol>'
+CAPTION_TAG = '<eob>'
+LINE_HOLDER = 'µ'
+CAPTION_HOLDER = '§'
+
+
+def ter_preprocess(infile, remove_eol=False, remove_eob=False, replace=False):
+    tagged_txt = preprocess(infile, line_tag=LINE_TAG, caption_tag=CAPTION_TAG, line_holder=LINE_HOLDER,
+                            caption_holder=CAPTION_HOLDER)
+    if remove_eol:
+        tagged_txt = re.sub(LINE_HOLDER, r" ", tagged_txt)
+    if remove_eob:
+        tagged_txt = re.sub(CAPTION_HOLDER, r" ", tagged_txt)
+    if replace:
+        tagged_txt = re.sub(LINE_HOLDER, CAPTION_HOLDER, tagged_txt)
+
+    # Inserting spaces besides boundaries
+    tagged_txt = re.sub(r"(%s|%s)" % (LINE_HOLDER, CAPTION_HOLDER), r" \1 ", tagged_txt)
+    # Removing potential multiple spaces
+    tagged_txt = re.sub(r" {2,}", r" ", tagged_txt)
+
+    tagged_sents = tagged_txt.splitlines()
+    tagged_sents = [tagged_sent.strip() for tagged_sent in tagged_sents]
+    masked_sents = [re.sub(r"[^ %s%s]+" % (LINE_HOLDER, CAPTION_HOLDER), MASK_CHAR, tagged_sent) for tagged_sent in tagged_sents]
+
+    return masked_sents
 
 
 def calculate_ter(sys, ref):
     # Calculates TER between masked system output and masked reference
-    ref1 = [(ref)]
-    ter = TER(ref1)
-    score = ter.corpus_score(sys, ref1)
-    return score
+    ref1 = [ref]
+    ter = TER()
+    ter_score = ter.corpus_score(sys, ref1)
+    signature = ter.get_signature()
+    return ter_score, signature
 
 
-def ter_process(reference_file, system_file):
-    ref = mask(reference_file, replace=False)
-    sys = mask(system_file, replace=False)
-    ter_score = calculate_ter(sys, ref)
-    print('TER score on masked text: ' + str(ter_score))
-    tr = float(str(ter_score).split('=')[1].lstrip())
-    return tr
+def ter_process(reference_file, system_file, extra=False):
+    ter = TER()
+
+    ref = ter_preprocess(reference_file)
+    sys = ter_preprocess(system_file)
+
+    ter_score = ter.corpus_score(sys, [ref])
+    signature = ter.get_signature()
+
+    if extra:
+        print('TER score on masked text:', ter_score)
+
+    print(signature)
+    return ter_score.score
 
